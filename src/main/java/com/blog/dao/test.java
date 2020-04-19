9bf0bb08f693c7305ac9957fb9baf2a98f638234
zhum <786200288@qq.com>
2020-04-19 12:52:19 +0800

    Initial commit

---
+++ src/main/java/com/blog/dao/test.java
@@ -0,0 +1,9 @@
+package com.blog.dao;
+
+import com.blog.entity.Top50;
+
+import java.util.List;
+
+public interface test {
+    List<Top50> listAll();
+}
---
+++ src/main/java/com/blog/entity/Top50.java
@@ -0,0 +1,13 @@
+package com.blog.entity;
+
+import lombok.Data;
+
+@Data
+public class Top50 {
+    public String animename;
+    public String follwnum;
+    public String pts;
+    public Top50(){
+
+    }
+}
